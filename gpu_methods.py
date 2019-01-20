2ed288f90c3b4509634f075d3ebfeefb549082b3
Egor Nemchinov <immortalsuperturtle@gmail.com>
2019-01-20 14:41:27 +0300

    Multiplication for packed on both axis bits

--- gpu_methods.py
+++ gpu_methods.py
@@ -6,6 +6,7 @@ threadsperblock = (16, 16)
 blockspergrid = None
 is_changed = cuda.device_array((1,), dtype=bool)
 
+
 @cuda.jit
 def matmul(A, B, C):
     """Perform matrix multiplication of C = A * B
@@ -20,25 +21,49 @@ def matmul(A, B, C):
             C[row, col] = tmp
 
 
+@cuda.jit
+def matmul_packed_both(A, B, C):
+    """Perform matrix multiplication of C = A * B
+    with packed bits on both axis
+    """
+    row, col = cuda.grid(2)
+    if row < C.shape[0] and col < C.shape[1]:
+        value = 0
+        for j in range(8):
+            tmp = 0
+            for k in range(A.shape[1]):
+                if col + j >= B.shape[1]:
+                    return
+                tmp = tmp | (A[row, k] & B[k, col + j])
+            value |= tmp << j
+        if value != C[row, col]:
+            is_changed[0] = True
+            C[row, col] |= value
+
+
 def matrices_to_gpu(matrices):
     for nonterminal, matrix in matrices.items():
-        assert str(matrix.dtype) == 'bool', 'Matrix dtype is {}'.format(matrix.dtype)
+        # assert str(matrix.dtype) == 'bool', 'Matrix dtype is {}'.format(matrix.dtype)
         matrices[nonterminal] = cuda.to_device(matrix)
     return matrices
 
 
 def matrices_from_gpu(matrices):
     for nonterminal, matrix in matrices.items():
-        assert str(matrix.dtype) == 'bool', 'Matrix dtype is {}'.format(matrix.dtype)
+        # assert str(matrix.dtype) == 'bool', 'Matrix dtype is {}'.format(matrix.dtype)
         matrices[nonterminal] = matrix.copy_to_host()
     return matrices
 
 
-def update_matrix_gpu(head_mat, body_first_mat, body_second_mat):
-    # new_matrix = cuda.device_array(head_mat.shape, dtype=bool)
-    is_changed[0] = False
+def update_matrix_gpu(head_mat, body_first_mat, body_second_mat, packed_y=False):
     blockspergrid_x = int(math.ceil(body_first_mat.shape[0] / threadsperblock[0]))
     blockspergrid_y = int(math.ceil(body_second_mat.shape[1] / threadsperblock[1]))
     blockspergrid = (blockspergrid_x, blockspergrid_y)
-    matmul[blockspergrid, threadsperblock](body_first_mat, body_second_mat, head_mat)
+    is_changed[0] = False
+    if str(head_mat.dtype) == 'bool':
+        matmul[blockspergrid, threadsperblock](body_first_mat, body_second_mat, head_mat)
+    elif str(head_mat.dtype) == 'uint8' and packed_y:
+        matmul_packed_both[blockspergrid, threadsperblock](body_first_mat, body_second_mat, head_mat)
+    else:
+        raise ValueError('Type {} is not supported'.format(head_mat.dtype))
     return head_mat if is_changed.copy_to_host() else None
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from gpu_methods import matrices_to_gpu, matrices_from_gpu, update_matrix_gpu
 from collections import defaultdict
 from parsing_utils import parse_grammar, parse_graph, products_set
+from copy import deepcopy
 import time
 
 silent = False
@@ -14,19 +15,28 @@ def log(message):
         print(message)
 
 
-def main(grammar_file, graph_file):
+def main(grammar_file, graph_file, type='bool', pack_both_axis=False):
     t_start = t_parse_start = time.time()
     grammar, inverse_grammar = parse_grammar(grammar_file)
     graph, nodes_amount = parse_graph(graph_file)
 
     t_parse_end = t_bool_adj_start = time.time()
     matrices = get_boolean_adjacency_matrices(grammar, inverse_grammar, graph, nodes_amount)
+    matrices_packed_y = deepcopy(matrices) if pack_both_axis else None
+    if type != 'bool':
+        matrices_to_type(matrices, type)
+        if matrices_packed_y is not None:
+            matrices_to_type(matrices_packed_y, type)
+    if on_gpu:
+        matrices = matrices_to_gpu(matrices)
 
     t_bool_adj_end = t_solution_start = time.time()
     remove_terminals(grammar, inverse_grammar)
-    iterate_on_grammar(grammar, inverse_grammar, matrices)
+    iterate_on_grammar(grammar, inverse_grammar, matrices, matrices_packed_y=matrices_packed_y)
     if on_gpu:
         matrices = matrices_from_gpu(matrices)
+    if type != 'bool':
+        matrices = matrices_from_type(matrices, type, nodes_amount)
 
     t_solution_end = time.time()
     print(solution_string(matrices))
@@ -35,6 +45,25 @@ def main(grammar_file, graph_file):
     log(f'Getting adjacent matrices took {t_bool_adj_end - t_bool_adj_start} s')
     log(f'Solving took {t_solution_end - t_solution_start} s')
     log(f'Total execution time (with print) is {t_end - t_start} s')
+
+
+def matrices_to_type(matrices, type, axis=-1):
+    if type in [np.uint8, 'uint8', 'byte']:
+        for key, matrix in matrices.items():
+            matrices[key] = np.packbits(matrix, axis=axis)
+    else:
+        raise ValueError('Casting to type {} is not supported yet'.format(type))
+    return matrices
+
+
+def matrices_from_type(matrices, type, nodes_amount, axis=-1):
+    if type in [np.uint8, 'uint8', 'byte']:
+        for key, matrix in matrices.items():
+            matrix = np.unpackbits(matrix, axis=axis)[:nodes_amount, :nodes_amount]
+            matrices[key] = matrix
+    else:
+        raise ValueError('Casting to type {} is not supported yet'.format(type))
+    return matrices
 
 
 def remove_terminals(grammar, inverse_grammar):
@@ -55,14 +84,11 @@ def get_boolean_adjacency_matrices(grammar, inv_grammar, graph, nodes_amount):
             if value in inv_grammar:
                 for nonterminal in inv_grammar[value]:
                     matrices[nonterminal][row, col] = True
-    if on_gpu:
-        matrices = matrices_to_gpu(matrices)
     log('Calculated {} adjacency matrices of shape {}'.format(len(matrices), (size, size)))
     return matrices
 
 
-def iterate_on_grammar(grammar, inverse_grammar, matrices):
-    # Needed for smarter iterating
+def iterate_on_grammar(grammar, inverse_grammar, matrices, matrices_packed_y=None):
     inverse_by_nonterm = defaultdict(set)
     for body, heads in inverse_grammar.items():
         assert type(body) is tuple, 'Left terminals in grammar: {}'.format(body)
@@ -75,22 +101,27 @@ def iterate_on_grammar(grammar, inverse_grammar, matrices):
     log('Built inverse_by_nonterm dictionary')
 
     to_recalculate = set(products_set(grammar))
+    packed_y = matrices_packed_y is not None
     while to_recalculate:
         head, body = to_recalculate.pop()
         assert type(body) is tuple, 'Body is either str or tuple, not {}'.format(type(body))
         matrix = matrices[head]
+        second_matrix = matrices_packed_y[body[1]] if packed_y else matrices[body[1]]
         if on_gpu:
-            new_matrix = update_matrix_gpu(matrix, matrices[body[0]], matrices[body[1]])
+            new_matrix = update_matrix_gpu(matrix, matrices[body[0]], second_matrix, packed_y=packed_y)
         else:
-            new_matrix = update_matrix_cpu(matrix, matrices[body[0]], matrices[body[1]])
+            new_matrix = update_matrix_cpu(matrix, matrices[body[0]], second_matrix, packed_y=packed_y)
         if new_matrix is not None:
             to_recalculate |= inverse_by_nonterm[head]
             matrices[head] = new_matrix
     log('Finished iterating on grammar. Final matrices are calculated')
 
 
-def update_matrix_cpu(head_mat, body_first_mat, body_second_mat):
-    new_matrix = head_mat + body_first_mat.dot(body_second_mat)
+def update_matrix_cpu(head_mat, body_first_mat, body_second_mat, packed_y=False):
+    if str(head_mat.dtype) == 'bool':
+        new_matrix = head_mat + body_first_mat.dot(body_second_mat)
+    else:
+        raise ValueError('Multiplication of matrices type {} is not supported'.format(head_mat.dtype))
 
     if np.all(new_matrix == head_mat):
         return None
@@ -114,8 +145,10 @@ if __name__ == '__main__':
     parser.add_argument('graph', type=str, help='Path to a directional graph')
     parser.add_argument('-s', '--silent', action='store_true', help='Print logs into console')
     parser.add_argument('-c', '--cpu', action='store_true', help='Run on CPU')
+    parser.add_argument('-t', '--type', type=str, default='bool', help='Compress bools to type')
+    parser.add_argument('-b', '--pack_both_axis', action='store_true', help='Store matrices packed by both axis')
     args = parser.parse_args()
     silent = args.silent
     on_gpu = not args.cpu
 
-    main(args.grammar, args.graph)
+    main(args.grammar, args.graph, type=args.type, pack_both_axis=args.pack_both_axis)
